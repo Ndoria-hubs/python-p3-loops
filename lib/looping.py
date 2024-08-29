4308bf780d32711beac8581a1278379df801fb53
Ndoria <k.ndoriamurigu@gmail.com>
2024-08-29 15:42:44 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 0
+    while count < 10:
+        count += 1
+        print(count)
+        if count == 10:
+            print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squarelist = []
+    for num in int_list:
+        newnum = num ** 2
+        squarelist.append(newnum)
+    return squarelist
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
